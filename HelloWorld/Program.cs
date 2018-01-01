865976fc00b9b7457638fdcb959481fb33d49ee8
ThePigg <joe_tidey@hotmail.co.uk>
2018-01-01 15:08:37 +0000

    Experimenting

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,8 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
 
 namespace HelloWorld
 {
@@ -10,6 +10,21 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
+            var counter = 0;
+            while (counter <= 5)
+            {
+                ForLoop((counter + 1) * 5);
+                counter++;
+            }
+            Console.ReadLine();
+        }
+
+        static void ForLoop(int count)
+        {
+            for (int i = 0; i <= count; i++)
+            {
+                Console.WriteLine(String.Format("I would like {0} doob(s) pls.", i));
+            }
         }
     }
 }
